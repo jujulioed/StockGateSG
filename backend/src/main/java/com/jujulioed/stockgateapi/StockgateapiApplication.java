66c5adc20242eeae941a54f53c23b0254112555e
jujulioed <julioeduardon9@hotmail.com>
2026-07-11 23:50:35 -0300

    Start backend

---
+++ backend/src/main/java/com/jujulioed/stockgateapi/StockgateapiApplication.java
@@ -0,0 +1,13 @@
+package com.jujulioed.stockgateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockgateapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockgateapiApplication.class, args);
+	}
+
+}
